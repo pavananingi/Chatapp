986793da73de074ed0c85c8131d350e66616182f
pavananingi <116421166+pavananingi@users.noreply.github.com>
2024-08-14 11:34:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,33 +8,48 @@ app.use(express.static(__dirname + "/public"));
 
 // homepage
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 // socket connection
 io.on('connection', (socket) => {
-	console.log("A user connected!");
+    console.log("A user connected!");
 
-	// executes when a user starts typing
-	socket.on('userTyping', (username) => {
-		socket.broadcast.emit('userTyping', username);
-	});
+    // Executes when a user starts typing
+    socket.on('userTyping', (username) => {
+        socket.broadcast.emit('userTyping', username);
+    });
 
-	// executes when a user sends a message
-	socket.on('msg', (msg) => {
-		console.log("message: ", msg);
-		socket.broadcast.emit('msg', msg);
-	});
+    // Executes when a user sends a message
+    socket.on('msg', (msg) => {
+        console.log("message: ", msg);
+        socket.broadcast.emit('msg', msg);
+    });
 
-	// executes when a new user joins the chat room
-	socket.on('join', (username) => {
-		socket.broadcast.emit('join', username);
-	});
+    // Executes when a new user joins the chat room
+    socket.on('join', (username) => {
+        socket.broadcast.emit('join', username);
+    });
+
+    // Executes when a user sends a voice message
+    socket.on('voiceMsg', (data) => {
+        console.log("voice message received from user:", data.user);
+        // Broadcast the voice message to all other users
+        socket.broadcast.emit('voiceMsg', {
+            user: data.user,
+            audio: data.audio
+        });
+    });
+
+    // Disconnect event
+    socket.on('disconnect', () => {
+        console.log("A user disconnected");
+    });
 });
 
 const PORT = process.env.PORT || 5000;
 
 // port to listen
 http.listen(PORT, function(){
-  console.log('listening on port: ', PORT);
+    console.log('listening on port:', PORT);
 });
